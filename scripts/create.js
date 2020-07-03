e0a2ec53375b5b5414471d34a1584b2a70227f69
xuzhen <richxu@dfocuspace.com>
2020-07-03 15:10:07 +0800

    feat: basciComponents

--- scripts/create.js
+++ scripts/create.js
@@ -2,76 +2,79 @@
  * Created by 51375 on 2017/7/8.
  */
 
-let fs = require('fs')
-let basepath = 'src/pages/'
-let moment = require('moment')
-let cptName = process.argv.splice(2)[0]
-let path = cptName.split('/')
-let name = path[path.length - 1]
-let writes = [`index.tsx`, `index.less`, `components`, `models`, `services`]
-let reads = [`basicComponents/index.tsx`, `basicComponents/index.less`]
-let file = []
-let author = require('os')
-  .homedir()
-  .split('\\')
-  .pop()
+let fs = require("fs");
+let basepath = "../src/views/";
+let moment = require("moment");
+let cptName = process.argv.splice(2)[0];
+let path = cptName.split("/");
+let name = path[path.length - 1];
+let writes = [`index.tsx`, `index.less`, `components`, `models`, `services`];
+let reads = [`basicComponents/index.tsx`, `basicComponents/index.less`];
+let file = [];
+let author = require("os").homedir().split("\\").pop();
 
 //检测是否存在文件夹
-let exists = function() {
+let exists = function () {
   return new Promise((res, rej) => {
-    ;(async function() {
+    (async function () {
       for (let a of path) {
-        fs.existsSync(basepath + a) ? (basepath = `${basepath}${a}/`) : await mkdir(a)
+        fs.existsSync(basepath + a)
+          ? (basepath = `${basepath}${a}/`)
+          : await mkdir(a);
       }
-      res(basepath)
-    })()
-  })
-}
+      res(basepath);
+    })();
+  });
+};
 //建立文件夹
-let mkdir = function(a) {
+let mkdir = function (a) {
   return new Promise((res, rej) => {
-    fs.mkdir(basepath + a, err => {
-      if (err) rej(err)
-      basepath = `${basepath}${a}/`
-      res(basepath)
-    })
-  })
-}
+    fs.mkdir(basepath + a, (err) => {
+      if (err) rej(err);
+      basepath = `${basepath}${a}/`;
+      res(basepath);
+    });
+  });
+};
 //读取模板文件内容，并替换为目标组件
-let readFile = function() {
-  return new Promise(res => {
+let readFile = function () {
+  return new Promise((res) => {
     for (let a of reads) {
-      let text = fs.readFileSync(a).toString()
+      let text = fs.readFileSync(a).toString();
       text = text
-        .replace(/time/g, moment().format('YYYY/MM/DD'))
+        .replace(/time/g, moment().format("YYYY/MM/DD"))
         .replace(/temp/g, name)
-        .replace(/author/g, author)
-      file.push(text)
+        .replace(/author/g, author);
+      file.push(text);
     }
-    res(file)
-  })
-}
+    res(file);
+  });
+};
 //生成文件，并填入之前读取的文件内容
-let writeFile = function(file) {
+let writeFile = function (file) {
   return new Promise((res, rej) => {
-    ;(async function() {
+    (async function () {
       for (let a of writes) {
-        await fs.writeFile(`${basepath}${a}`, a == writes[3] ? file[0] : a == writes[0] ? file[1] : '', err => {
-          if (err) rej(err)
-        })
+        await fs.writeFile(
+          `${basepath}${a}`,
+          a == writes[3] ? file[0] : a == writes[0] ? file[1] : "",
+          (err) => {
+            if (err) rej(err);
+          }
+        );
       }
-      res('succ')
-    })()
-  })
-}
+      res("succ");
+    })();
+  });
+};
 async function creatCpt() {
   try {
-    await exists()
-    await readFile()
-    await writeFile(await readFile())
-    return console.log(`Successfully created ${name} component`)
+    await exists();
+    await readFile();
+    await writeFile(await readFile());
+    return console.log(`Successfully created ${name} component`);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
-creatCpt()
+creatCpt();
