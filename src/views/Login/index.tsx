0566ae44cc64c4f11d64823a57f2aeecd9d1d28a
promotion-xu <17521337708@163.com>
2022-03-05 23:23:28 +0800

    feat: resize

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
-
 import './index.less';
 
 class Login extends React.Component {
+
+  handleResizeChange() {
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResizeChange)
+  }
+
   render() {
     return (
       <div className="Login">Login</div>
